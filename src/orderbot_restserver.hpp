8348a2c53a2e6960a1a4cc103455dee6d9555904
lzxm160 <lzxm160@sina.com>
2016-04-13 10:04:10 +0800

    order bot server

--- src/orderbot_restserver.hpp
+++ src/orderbot_restserver.hpp
@@ -175,9 +175,18 @@ void get_orders_param_func(const std::shared_ptr< Session > session)
 	{
 		boost::shared_ptr<orderbot> order = boost::shared_ptr<orderbot>(new orderbot(get_config->m_orderbot_username, get_config->m_orderbot_password, get_config->m_orderbot_url));
 		order->request("GET", path, param, "");
+		/////////////////////////////////////
+		BOOST_LOG_SEV(slg, boost_log->get_log_level()) << "request:GET "<<path<<":"<<param<<":";
+		boost_log->get_initsink()->flush();
+		/////////////////////////////////////////////////////
 		//cout<<order->get_data().length()<<":"<<order->get_data()<<endl;
 		cout<<"status:"<<order->get_status()<<endl;
 		session->close(order->get_status(), order->get_data(), { {"Cache-Control","no-cache"},{"Pragma","no-cache"},{ "Content-Type", "application/json; charset=utf-8" },{ "Content-Length", ::to_string(order->get_data().length()) } });
+
+		////////////////////////////////////////////////////////////
+		BOOST_LOG_SEV(slg, boost_log->get_log_level()) << "response:"<<order->get_status()<<":"<<order->get_data();
+		boost_log->get_initsink()->flush();
+		/////////////////////////////////////////////////////
 	});
 }
 void get_orders_num_func(const std::shared_ptr< Session > session)
