c440dd7918eb7f6826de7c12e0ba0d2183f16b58
lzxm160 <lzxm160@sina.com>
2016-04-18 17:17:54 +0800

    get product all

--- src/product_inventory.hpp
+++ src/product_inventory.hpp
@@ -45,9 +45,9 @@ public:
 				ptree child = sub.second.get_child("inventory_quantities");
 				for(auto& subchild:child)
 				{
-					//long distribution_center_id=subchild.second.get<long>("distribution_center_id");
+					long distribution_center_id=subchild.second.get<long>("distribution_center_id");
 					string distribution_center_name=subchild.second.get<string>("distribution_center_name");
-					//long inventory_quantity=subchild.second.get<long>("inventory_quantity");
+					long inventory_quantity=subchild.second.get<long>("inventory_quantity");
 					//cout<<distribution_center_name<<":"<<inventory_quantity<<endl;
 				}
 			}
