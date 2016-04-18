7f93fcc831ef77e1d53aa8c68648a8e485587b38
lzxm160 <lzxm160@sina.com>
2016-04-18 17:47:38 +0800

    get product all

--- src/product_inventory.hpp
+++ src/product_inventory.hpp
@@ -43,17 +43,17 @@ public:
 				cout<<product_name<<":"<<sku<<endl;
 				
 				ptree child = sub.second.get_child("inventory_quantities");
+
 				if(!child.empty())
 				{
-					cout<<"empty"<<endl;
-				
 					for(auto& subchild:child)
 					{
-						cout<< "{" << subchild.first << "}" << "[" << subchild.second.data() << "]\n";  
+					
 						// long distribution_center_id=subchild.second.get<long>("distribution_center_id");
-						// string distribution_center_name=subchild.second.get<string>("distribution_center_name");
+						string distribution_center_name=subchild.second.get<string>("distribution_center_name");
 						// long inventory_quantity=subchild.second.get<long>("inventory_quantity");
 						//cout<<distribution_center_name<<":"<<inventory_quantity<<endl;
+						cout<<distribution_center_name<<endl;
 					}
 				}
 			}
