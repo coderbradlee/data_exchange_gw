755842a3f17dffcefbe909299322b663986d228b
lzxm160 <lzxm160@sina.com>
2016-04-20 14:07:23 +0800

    add order listener

--- src/main.cpp
+++ src/main.cpp
@@ -10,14 +10,14 @@ int main()
 {
 	try
 	{
-		{
+		{	//failover://(tcp://172.18.100.204:61616)
 			order_activemq t;
-			t.start_consume_listener();
+			//t.start_consume_listener();
 			// this_thread::sleep_for(chrono::seconds(10));
 		}
 		{
 			product_inventory t;
-			//t.start_update();
+			t.start_update();
 			//t.start_update();
 		}
 		{
