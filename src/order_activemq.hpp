34b86ed96ec67dcce8fed26ce55552790237ec11
lzxm160 <lzxm160@sina.com>
2016-04-20 14:15:14 +0800

    add order listener

--- src/order_activemq.hpp
+++ src/order_activemq.hpp
@@ -10,6 +10,8 @@
 class order_activemq
 {
 public:
+	order_activemq(){}
+
 	void start_consume_listener()
 	{
 		std::string brokerURI =
@@ -20,29 +22,34 @@ public:
 //        "&transport.tcpTracingEnabled=true"
 //        "&wireFormat.tightEncodingEnabled=true"
         ")";
-    bool useTopics = false;
+	    bool useTopics = false;
 
-    //============================================================
-    // set to true if you want the consumer to use client ack mode
-    // instead of the default auto ack mode.
-    //============================================================
-    bool clientAck = false;
+	    //============================================================
+	    // set to true if you want the consumer to use client ack mode
+	    // instead of the default auto ack mode.
+	    //============================================================
+	    bool clientAck = false;
 
-    // Create the consumer
-    activemq_cms_consumer consumer( brokerURI, get_config->m_activemq_read_order_queue, useTopics, clientAck );
-    //cout<<__FILE__<<":"<<__LINE__<<endl;
-    // Start it up and it will listen forever.
-    consumer.runConsumer();
+	    // Create the consumer
+	    activemq_cms_consumer consumer( brokerURI, get_config->m_activemq_read_order_queue, useTopics, clientAck );
+	    //cout<<__FILE__<<":"<<__LINE__<<endl;
+	    // Start it up and it will listen forever.
+	    consumer.runConsumer();
 
-    // Wait to exit.
-    std::cout << "Press 'q' to quit" << std::endl;
-    while( std::cin.get() != 'q') {}
+	    // Wait to exit.
+	    std::cout << "Press 'q' to quit" << std::endl;
+	    while( std::cin.get() != 'q') {}
 
-    // All CMS resources should be closed before the library is shutdown.
-    consumer.close();
+	    // All CMS resources should be closed before the library is shutdown.
+	    consumer.close();
 
-    activemq::library::ActiveMQCPP::shutdownLibrary();
-	}
+	    activemq::library::ActiveMQCPP::shutdownLibrary();
+		}
+		private:
+		//boost::shared_ptr<MySql> m_conn;
+		string m_today_string;
+		//boost::shared_ptr<string> m_product_all;
+		std::stringstream m_ss;
 };
 
 #endif
