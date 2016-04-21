e3fab9350dad26d70b739e866388b4147bfb2744
lzxm160 <lzxm160@sina.com>
2016-04-21 11:34:03 +0800

    multithread activemq consumer

--- src/main.cpp
+++ src/main.cpp
@@ -13,8 +13,8 @@ int main()
 		{	//failover://(tcp://172.18.100.204:61616)
 			//failover://(tcp://172.18.100.204:61616)
 			order_activemq t;
-			//t.start_consume_listener_multithread();
-			t.start_consume_listener();//ok
+			t.start_consume_listener_multithread();
+			//t.start_consume_listener();//ok
 			// this_thread::sleep_for(chrono::seconds(10));
 		}
 		{
