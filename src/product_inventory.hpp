fd8570e8bc6dfd9e914be4d53c18a9adb860dcd3
lzxm160 <lzxm160@sina.com>
2016-04-21 15:11:01 +0800

    multithread activemq consumer

--- src/product_inventory.hpp
+++ src/product_inventory.hpp
@@ -7,7 +7,7 @@
 #include "orderbot.hpp"
 #include "activemq_cms.hpp"
 
-class product_inventory:public boost::enable_shared_from_this<product_inventory>
+class product_inventory:virtual public boost::enable_shared_from_this<product_inventory>
 {
 public:
 	product_inventory():m_d_t(m_io_s)
