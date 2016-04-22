ab40af327c66e03c7835b721f3af28a338c87d72
lzxm160 <lzxm160@sina.com>
2016-04-22 08:57:19 +0800

    producer and consumer ok

--- src/activemq_producer_consumer.hpp
+++ src/activemq_producer_consumer.hpp
@@ -11,35 +11,19 @@
 class activemq_producer_consumer
 {
 public:
-	
+
 	void start()
 	{
 		activemq::library::ActiveMQCPP::initializeLibrary();
-		// boost::shared_ptr<order_activemq> consume(new order_activemq);		
-		// thread consume_thread([&consume](){consume->start();});	
-		//////////////////////////
-		// boost::shared_ptr<product_inventory> produce(new product_inventory);
-		// thread produce_thread([&produce](){produce->start();});	
-		
-		// produce_thread.join();
-		//consume_thread.join();
-		
 
 		boost::shared_ptr<product_inventory> producer(new product_inventory);
         boost::shared_ptr<order_activemq> consumer(new order_activemq);
 
 	    // Start the consumer thread.
 	    thread consumerThread([&consumer](){consumer->start();});
-	    //consumerThread.start();
 
-	    // Wait for the consumer to indicate that its ready to go.
-	    //consumer->waitUntilReady();
-
-	    // Start the producer thread.
 	    thread producerThread([&producer](){producer->start();});
-	    //producerThread.start();
 
-	    // Wait for the threads to complete.
 	    producerThread.join();
 	    consumerThread.join();
 		activemq::library::ActiveMQCPP::shutdownLibrary();
