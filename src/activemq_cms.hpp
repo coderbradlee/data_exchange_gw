ab9955163b9d6754690391ed1dbe078bb6265a63
lzxm160 <lzxm160@sina.com>
2016-05-06 17:47:05 +0800

    change jsonspirit to nolmannjson

--- src/activemq_cms.hpp
+++ src/activemq_cms.hpp
@@ -477,9 +477,9 @@ public:
      ret_json_all["order_notes"]= note ;
      ret_json_all["internal_notes"]= "test internal";
      ret_json_all["bill_third_party"]= false;
-     ret_json_all["distribution_center_id"]= 3;
+     //ret_json_all["distribution_center_id"]= 3;
      cout<<dispatch_warehouse_id<<":"<<__FILE__<<":"<<__LINE__<<endl;
-     //ret_json_all["distribution_center_id"]= dispatch_warehouse_id;//need get from orderbot
+     ret_json_all["distribution_center_id"]= dispatch_warehouse_id;//need get from orderbot
     //  ret_json_all["account_group_id"]= nullptr;//need get from orderbot
     //  ret_json_all["order_guide_id"]= nullptr;//need get from orderbot
     //  ret_json_all["insure_packages"]= false;//not sure
