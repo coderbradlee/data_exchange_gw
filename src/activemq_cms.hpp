2a424a83f95b586f0de0842d0a14cc9475eb0c76
lzxm160 <lzxm160@sina.com>
2016-05-06 16:31:12 +0800

    change jsonspirit to nolmannjson

--- src/activemq_cms.hpp
+++ src/activemq_cms.hpp
@@ -415,7 +415,7 @@ public:
         string text="{    \"sales_order_id\": \"1\",    \"so_no\": \"2\",    \"po_no\": \"3\",    \"status\": 0,    \"order_date\": \"2016-05-03\",    \"company_id\": \"4\",    \"sales_id\": \"5\",    \"currency_id\": \"6\",    \"ss_currency_daily_exchange_rate\": 6.45,    \"tax_schedule_id\": \"7\",    \"ss_tax_rate\": 7.2,    \"customer_master_id\": \"8\",    \"customer_contact_id\": \"9\",    \"customer_invoice_address_id\": \"10\",    \"ship_to_customer_name\": \"11\",    \"ship_to_address\": \"12\",    \"ship_to_state\": \"NY\",    \"ship_to_city\": \"13\",    \"ship_to_zip_code\": \"10118\",    \"ship_to_contact_name\": \"14\",    \"ship_to_contact_phone_number\": \"+1 800-428-4322\",    \"ship_to_contact_email\": \"test@orderbot.com\",    \"trade_term_id\": \"17\",    \"ss_landed_cost_coefficient\": 3.3,    \"dispatch_warehouse_id\": 1,    \"requested_delivery_date\": \"2016-05-03\",    \"promotion_code\": \"\",    \"company_bank_account_id\": \"\",    \"shipping_cost_total\": 25.48,    \"saving_total\": 3.56,    \"tax_total\": 22.51,    \"sub_total\": 180.37,    \"grand_total\": 218.67,    \"note\": \"note\",    \"detail\": [        {            \"sales_order_detail_id\": 44,            \"item_master_id\": \"\",            \"ss_guidance_price\": 5.46,            \"ss_promotion_price\": 5.41,            \"unit_price\": 5.43,            \"uom_id\": \"\",            \"quantity\": 12,            \"sub_total\": 63.67,            \"sub_tax\": 4.33,            \"sub_shipping_cost\": 5.68,            \"sub_discount\": 0,            \"note\": \"detail.note\"        }    ]}";
         const auto& j = json::parse(text);
         cout<<j<<endl;
-        const auto& sales_order_id = j.find("sales_order_id");
+       // const auto& sales_order_id = j.find("sales_order_id");
     // const auto& sales_order_id = j["sales_order_id"];
     // const auto& so_no = j["so_no"];
     // const auto& po_no = j["po_no"];
@@ -457,7 +457,7 @@ public:
      // ret_json_all["orderbot_account_id"]= nullptr;
      // ret_json_all["order_date"]=order_date ;
      // ret_json_all["ship_date"]=requested_delivery_date ;
-     // ret_json_all["orderbot_customer_id"]="need get from orderbot";//need get from orderbot
+     ret_json_all["orderbot_customer_id"]="need get from orderbot";//need get from orderbot
      // ret_json_all["reference_customer_id"]=customer_master_id ;
      // ret_json_all["reference_order_id"]=sales_order_id ;
      // ret_json_all["customer_po"]= sales_order_id ;
