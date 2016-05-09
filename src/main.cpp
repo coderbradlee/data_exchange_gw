c6f4c0077e0e407f90716ab2e86de2445aeef4f2
lzxm160 <lzxm160@sina.com>
2016-05-09 15:00:47 +0800

    test class

--- src/main.cpp
+++ src/main.cpp
@@ -60,7 +60,7 @@ namespace brad
 		}
 		void print() const
 		{
-			cout<<m_year<<"-"<<setw(2)<<m_month<<"-"<<m_day<<endl;
+			cout<<m_year<<"-"<<setw(2)<< setfill ('0')<<m_month<<setw(2)<< setfill ('0')<<"-"<<m_day<<endl;
 		}
 		private:
 		int m_year;
