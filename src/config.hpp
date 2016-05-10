8894e3773c4c52705fad62602c520da16e66d695
lzxm160 <lzxm160@sina.com>
2016-05-10 16:31:46 +0800

    add exchange rate

--- src/config.hpp
+++ src/config.hpp
@@ -42,8 +42,8 @@ class iconfig:public boost::enable_shared_from_this<iconfig>, boost::noncopyable
 			m_activemq_username = m_pt.get<std::string>("activemq.username");
 			m_activemq_password = m_pt.get<std::string>("activemq.password");
 			m_activemq_url = m_pt.get<std::string>("activemq.url");	
-			m_activemq_read_queue = m_pt.get<std::string>("activemq.read_queue");
-			m_activemq_write_queue = m_pt.get<std::string>("activemq.write_queue");	
+			// m_activemq_read_queue = m_pt.get<std::string>("activemq.read_queue");
+			// m_activemq_write_queue = m_pt.get<std::string>("activemq.write_queue");	
 			m_activemq_write_product_queue= m_pt.get<std::string>("activemq.write_product_queue");
 			m_write_product_interval = boost::lexical_cast<size_t>(m_pt.get<std::string>("activemq.write_product_interval"));	
 			m_activemq_read_order_queue= m_pt.get<std::string>("activemq.read_order_queue");	
