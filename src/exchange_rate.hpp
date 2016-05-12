6645274aaa14d72ba345631beb648609f93eb9ca
lzxm160 <lzxm160@sina.com>
2016-05-12 14:20:21 +0800

    change exchange rate api

--- src/exchange_rate.hpp
+++ src/exchange_rate.hpp
@@ -418,7 +418,7 @@ public:
 			//SKW TRL RUR PLZ
 			for(std::vector<exchage_rate_data>::iterator item=m_exchage_rate_data_array.begin();item!=m_exchage_rate_data_array.end();++item)
 			{
-				if(item->code=="SKW"||item->code=="TRL"||item->code=="RUR"||item->code=="PLZ"||item->code=="MXP")
+				if(item->code=="SKW"||item->code=="TRL"||item->code=="RUR"||item->code=="PLZ"||item->code=="MXP"||item->code=="ROL")
 				{
 					m_exchage_rate_data_array.erase(item);
 				}
