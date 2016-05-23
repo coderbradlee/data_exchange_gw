eb540671c962e7c14165d75cdf167bdd8ee3ee80
lzxm160 <lzxm160@sina.com>
2016-05-23 11:01:42 +0800

    test nvi

--- src/test.hpp
+++ src/test.hpp
@@ -1130,7 +1130,7 @@ namespace design_model
     		// delete d;
     		derived d1(calc_func);
     		d1.health_value();
-    		derived d2(calc_func_object());
+    		derived d2(calc_func_object()());
     		d2.health_value();
     		// calc_class_member c;
     		// derived d3(std::bind(&calc_class_member::health,c,_1));
