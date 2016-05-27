30b6227cafe5adf691920e602f772c7356a32bd0
lzxm160 <lzxm160@sina.com>
2016-05-27 14:18:59 +0800

    test virtual fun address

--- src/test2.hpp
+++ src/test2.hpp
@@ -353,8 +353,8 @@ namespace x2
     		cout<<"base first function in v_table is:"<<test_fun_ptr<<endl;
 			cout<<"-------------------------------"<<endl;
 			cout<<*((int*)&b)<<endl;
-			typedef void(base::*member_fun)(void);
-			member_fun=&base::f;
+			//typedef void(base::*member_fun)(void);
+			void(base::*member_fun)(void)=&base::f;
 			//member_fun(*((int*)&b));
     		//cout<<"base first function in v_table is:"<<((int*)*)(int*)(&b)<<endl;
     		//cout<<"base first fun in v_table is:"<<(fun)(*(fun)(&b))<<endl;
