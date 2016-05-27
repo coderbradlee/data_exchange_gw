379e623ffe95c885fd2bb2e499d1c6bee9740021
lzxm160 <lzxm160@sina.com>
2016-05-27 13:14:50 +0800

    test virtual fun address

--- src/test2.hpp
+++ src/test2.hpp
@@ -341,7 +341,7 @@ namespace x2
 
     		cout<<"base vptr address is:"<<(int*)(&b)<<endl;
     		
-    		auto test_fun_ptr=(fun)(*(*((int*)(&b))));
+    		auto test_fun_ptr=(fun)(*((int*)(&b)));
     		cout<<typeid(test_fun_ptr).name()<<endl;
 
     		cout<<sizeof(test_fun_ptr)<<endl;
