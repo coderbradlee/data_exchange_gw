80fb9e7bad16fc42d55725251dc4ed67caf26b1e
lzxm160 <lzxm160@sina.com>
2016-05-31 10:03:38 +0800

    test array address

--- src/test2.hpp
+++ src/test2.hpp
@@ -340,8 +340,10 @@ namespace x2
     		typedef void(*fun)(void);
     		base *b=new base();
     		fun p_fun=nullptr;
-    		(*(((intptr_t*)b)[1]))();
-    		(((intptr_t*)b)[2])();
+    		p_fun=(*(((intptr_t*)b)[1]));
+    		p_fun();
+    		p_fun=(((intptr_t*)b)[2]);
+    		p_fun();
     		cout<<"---------------------"<<endl;
     		cout<<"base v_table address is:"<<(intptr_t*)(b)<<endl;
     		cout<<"base v_table address is:"<<b<<endl;
