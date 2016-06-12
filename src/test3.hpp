96e3a857289f924507498f9648fc0040c5a64589
lzxm160 <lzxm160@sina.com>
2016-06-12 17:29:40 +0800

    test locale

--- src/test3.hpp
+++ src/test3.hpp
@@ -1364,6 +1364,65 @@ namespace x3
             cout<<greater<int>()(100,300)<<endl;
         }
     }
+    namespace test_locale
+    {
+        void test()
+        {
+             using namespace boost::locale;
+            using namespace std;
+
+            generator gen;
+            // Make system default locale global
+            std::locale loc = gen("");
+            locale::global(loc); 
+            cout.imbue(loc);
+            
+
+            string text="Hello World! あにま! Linux2.6 and Windows7 is word and number. שָלוֹם עוֹלָם!";
+
+            cout<<text<<endl;
+
+            boundary::ssegment_index index(boundary::word,text.begin(),text.end());
+            boundary::ssegment_index::iterator p,e;
+
+            for(p=index.begin(),e=index.end();p!=e;++p) {
+                cout<<"Part ["<<*p<<"] has ";
+                if(p->rule() & boundary::word_number)
+                    cout<<"number(s) ";
+                if(p->rule() & boundary::word_letter)
+                    cout<<"letter(s) ";
+                if(p->rule() & boundary::word_kana)
+                    cout<<"kana character(s) ";
+                if(p->rule() & boundary::word_ideo)
+                    cout<<"ideographic character(s) ";
+                if(p->rule() & boundary::word_none)
+                    cout<<"no word characters";
+                cout<<endl;
+            }
+
+            index.map(boundary::character,text.begin(),text.end());
+
+            for(p=index.begin(),e=index.end();p!=e;++p) {
+                cout<<"|" <<*p ;
+            }
+            cout<<"|\n\n";
+
+            index.map(boundary::line,text.begin(),text.end());
+
+            for(p=index.begin(),e=index.end();p!=e;++p) {
+                cout<<"|" <<*p ;
+            }
+            cout<<"|\n\n";
+
+            index.map(boundary::sentence,text.begin(),text.end());
+
+            for(p=index.begin(),e=index.end();p!=e;++p) {
+                cout<<"|" <<*p ;
+            }
+            cout<<"|\n\n";
+            
+        }
+    }
 	void test()
 	{
 		
@@ -1381,7 +1440,8 @@ namespace x3
         //test_vector::test();
         //test_remove_if::test();
         //test_set::test();
-        test_function_object::test();
+        //test_function_object::test();
+        test_locale::test();
 	}
 }
 }
