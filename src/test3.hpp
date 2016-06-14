d6f2af62be5c918e5ad1788a30b33e78e34cd275
lzxm160 <lzxm160@sina.com>
2016-06-14 16:12:09 +0800

    test io_service post

--- src/test3.hpp
+++ src/test3.hpp
@@ -1569,7 +1569,7 @@ namespace x3
                 //one.post(boost::bind(func,i));
                 one.wrap(boost::bind(func,i));
             }
-            io.post(one);
+            //io.post(one);
             // for(int i=5;i<10;++i)
             // {
             //     io.post(two.wrap(boost::bind(func,i)));
@@ -1579,6 +1579,77 @@ namespace x3
                 th.create_thread([&](){io.run();});
             boost::this_thread::sleep(boost::posix_time::millisec(2000));
             th.join_all();
+        }
+    }
+    namespace test_strand_io
+    {
+        boost::mutex global_stream_lock;  
+  
+        void WorkerThread( boost::shared_ptr< boost::asio::io_service > io_service )  
+        {  
+            global_stream_lock.lock();  
+            std::cout << "[" << boost::this_thread::get_id()  
+                << "] Thread Start" << std::endl;  
+            global_stream_lock.unlock();  
+          
+            io_service->run();  
+          
+            global_stream_lock.lock();  
+            std::cout << "[" << boost::this_thread::get_id()  
+                << "] Thread Finish" << std::endl;  
+            global_stream_lock.unlock();  
+        }  
+          
+        void Dispatch( int x )  
+        {  
+            global_stream_lock.lock();  
+            std::cout << "[" <<  boost::this_thread::get_id()  << "] "   
+                << __FUNCTION__  << " x = " << x <<  std::endl;  
+            global_stream_lock.unlock();  
+        }  
+          
+        void Post( int x )  
+        {  
+            global_stream_lock.lock();  
+            std::cout << "[" <<  boost::this_thread::get_id()  << "] "   
+                << __FUNCTION__  << " x = " << x <<  std::endl;  
+            global_stream_lock.unlock();  
+        }  
+          
+        void Run3( boost::shared_ptr< boost::asio::io_service > io_service )  
+        {  
+            for( int x = 0; x < 3; ++x )  
+            {  
+                io_service->dispatch( boost::bind( &Dispatch, x * 2 ) );  
+                io_service->post( boost::bind( &Post, x * 2 + 1 ) );  
+                boost::this_thread::sleep( boost::posix_time::milliseconds( 1000 ) );  
+            }  
+        }  
+        void test()
+        {
+            boost::shared_ptr< boost::asio::io_service > io_service(   
+                new boost::asio::io_service  
+            );  
+            boost::shared_ptr< boost::asio::io_service::work > work(  
+                new boost::asio::io_service::work( *io_service )  
+            );  
+          
+            global_stream_lock.lock();  
+            std::cout << "[" <<  boost::this_thread::get_id()    
+                << "] The program will exit when all  work has finished." <<  std::endl;  
+            global_stream_lock.unlock();  
+          
+            boost::thread_group worker_threads;  
+            for( int x = 0; x < 1; ++x )  
+            {  
+                worker_threads.create_thread( boost::bind( &WorkerThread, io_service ) );  
+            }  
+          
+            io_service->post( boost::bind( &Run3, io_service ) );  
+          
+            work.reset();  
+          
+            worker_threads.join_all();  
         }
     }
 	void test()
@@ -1602,7 +1673,8 @@ namespace x3
         //test_locale::test();
         //test_string_size::test();
         //test_test::test();
-        test_asio_post::test();
+        //test_asio_post::test();
+        test_strand_io::test();
 	}
 }
 }
