5403ea29e84016742d86b1f5f673e58755ad6258
lzxm160 <lzxm160@sina.com>
2016-06-21 10:04:26 +0800

    test for_each

--- src/test4.hpp
+++ src/test4.hpp
@@ -23,31 +23,36 @@ namespace x3
         };
         void test()
         {
-            std::vector<int> v{3,4,2,8,15,15,267};
-            for(const auto &n:v)
-            {
-                cout<<' '<<n;
-            }
-            cout<<endl;
-            std::for_each(v.begin(),v.end(),[](int& n){n++;});
-            for(const auto&n:v)
-            {
-                cout<<' '<<n;
-            }
-            cout<<endl;
-            sum s=std::for_each(v.begin(),v.end(),sum());
-            for(const auto&n:v)
-            {
-                cout<<' '<<n;
-            }
-            cout<<endl<<s.m_sum<<endl;
+            // std::vector<int> v{3,4,2,8,15,15,267};
+            // for(const auto &n:v)
+            // {
+            //     cout<<' '<<n;
+            // }
+            // cout<<endl;
+            // std::for_each(v.begin(),v.end(),[](int& n){n++;});
+            // for(const auto&n:v)
+            // {
+            //     cout<<' '<<n;
+            // }
+            // cout<<endl;
+            // sum s=std::for_each(v.begin(),v.end(),sum());
+            // for(const auto&n:v)
+            // {
+            //     cout<<' '<<n;
+            // }
+            // cout<<endl<<s.m_sum<<endl;
 
-            auto it=std::find_if(v.begin(),v.end(),std::bind(std::greater<int>(),std::placeholders::_1,10));
-            cout<<*it<<endl;
-            int n=std::count(v.begin(),v.end(),16);
-            cout<<n<<endl;
-            n=std::count_if(v.begin(),v.end(),std::bind(std::less<int>(),std::placeholders::_1,10));
-            cout<<n<<endl;
+            // auto it=std::find_if(v.begin(),v.end(),std::bind(std::greater<int>(),std::placeholders::_1,10));
+            // cout<<*it<<endl;
+            // int n=std::count(v.begin(),v.end(),16);
+            // cout<<n<<endl;
+            // n=std::count_if(v.begin(),v.end(),std::bind(std::less<int>(),std::placeholders::_1,10));
+            // cout<<n<<endl;
+            std::vector<int> v{3,4,2,8,15,15,267};
+            std::vector<int> v2{3,4,2,8,16,15,267};
+            typedef std::vector<int>::iterator it_type;
+            std::pair<it_type,it_type> p=std::mismatch(v.begin(),v.end(),v2.begin(),v2.end());
+            cout<<*p.first<<":"<<*p.second<<endl;
         }
     }
 	void test()
