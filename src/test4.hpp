e86fa124511e03c8df726664a55d6200e275d604
lzxm160 <lzxm160@sina.com>
2016-06-21 15:11:14 +0800

    test for_each

--- src/test4.hpp
+++ src/test4.hpp
@@ -228,8 +228,13 @@ namespace x3
             {
                 cout<<n<<" ";
             }cout<<endl;
-            v.erase(v.begin()+1);
+            //v.erase(v.begin()+1);
             pop_heap(v.begin(),v.end());
+            for(const auto& n:v)
+            {
+                cout<<n<<" ";
+            }cout<<endl;
+            v.erase(v.begin()+v.size()-1);
             for(const auto& n:v)
             {
                 cout<<n<<" ";
