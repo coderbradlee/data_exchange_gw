80abb20b42869c6adaee7942332164eaa2fe5b61
lzxm160 <lzxm160@sina.com>
2016-07-12 14:01:04 +0800

    solve insert many times bug

--- src/exchange_rate.hpp
+++ src/exchange_rate.hpp
@@ -1141,6 +1141,7 @@ public:
         	string hour_minute=hour+":"+minute;
         	if(hour_minute==get_config->m_exchange_rate_insert_time)
         	{
+        		boost::this_thread::sleep(boost::posix_time::millisec(60000));
         		cout<<__FILE__<<":"<<__LINE__<<endl;
 	        	if(m_conn==nullptr)
 	        	{
@@ -1153,8 +1154,7 @@ public:
         		update_cny();
         		m_conn->close();
         		m_conn=nullptr;
-        		boost::this_thread::sleep(boost::posix_time::millisec(60000));
-
+        		
         	}
         	
         	//cout<<"handle wait"<<endl;
