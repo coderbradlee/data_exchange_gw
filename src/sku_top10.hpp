458414505adbdef686bb3444657f59d3f35947b0
lzxm160 <lzxm160@sina.com>
2016-12-09 17:40:01 +0800

    skutop10

--- src/sku_top10.hpp
+++ src/sku_top10.hpp
@@ -82,13 +82,11 @@ public:
 		{
 			BOOST_LOG_SEV(slg, severity_level::error) <<"(exception:)" << e.what()<<":"<<__FILE__<<":"<<__LINE__<<endl;
 			boost_log->get_initsink()->flush();cout<<e.what()<<":"<<__FILE__<<":"<<__LINE__<<endl;
-			start();
 		}
 		catch(std::exception& e)
 		{
 			BOOST_LOG_SEV(slg, severity_level::error) <<"(exception:)" << e.what()<<":"<<__FILE__<<":"<<__LINE__<<endl;
 			boost_log->get_initsink()->flush();cout<<e.what()<<":"<<__FILE__<<":"<<__LINE__<<endl;
-			start();
 		}
 	}
 	void handle_wait(const boost::system::error_code& error)  
